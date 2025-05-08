0b07bc17bcf273642895683246503cf90a871ea9
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-05-08 18:53:37 +0800

    Update 199-diy.sh

--- patch/ipq-breeze/199-diy.sh
+++ patch/ipq-breeze/199-diy.sh
@@ -8,7 +8,9 @@ uci delete ttyd.@ttyd[0].interface
 # 设置所有网口可连接 SSH
 uci set dropbear.@dropbear[0].Interface=''
 
-
+uci set dhcp.lan.ra='server'
+uci set dhcp.lan.dns_service='0'
+uci commit dhcp
 uci commit
 
 #sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
