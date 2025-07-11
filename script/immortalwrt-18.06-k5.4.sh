1bcf3145e1104ebcb4aba60a6f2225418609a4d3
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-07-11 16:59:20 +0800

    Update immortalwrt-18.06-k5.4.sh

--- script/immortalwrt-18.06-k5.4.sh
+++ script/immortalwrt-18.06-k5.4.sh
@@ -3,7 +3,7 @@ sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/l
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #mv $GITHUB_WORKSPACE/patch/imm21.02/mac80211.sh $OPENWRT_PATH/package/kernel/mac80211/files/lib/wifi/mac80211.sh
-mv $GITHUB_WORKSPACE/patch/immortalwrt-21.02/zz-diy package/base-files/files/etc/uci-defaults/zz-diy
+mv $GITHUB_WORKSPACE/patch/immortalwrt-21.02/zz-diy-wrt1900 package/base-files/files/etc/uci-defaults/zz-diy
 sed -i 's#downloads.immortalwrt.org#mirror.nju.edu.cn/immortalwrt#g' package/emortal/default-settings/files/99-default-settings-chinese
 sed -i 's/ImmortalWrt/OpenWrt/g' include/version.mk
 
