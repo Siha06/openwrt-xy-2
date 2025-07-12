e3f5de7c3ab436f65435441cdf695d4649e4bf4b
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-07-12 13:28:57 +0800

    Update immortalwrt-18.06-k5.4.sh

--- script/immortalwrt-18.06-k5.4.sh
+++ script/immortalwrt-18.06-k5.4.sh
@@ -3,7 +3,7 @@ sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/l
 sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 sed -i 's/ImmortalWrt/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #mv $GITHUB_WORKSPACE/patch/imm21.02/mac80211.sh $OPENWRT_PATH/package/kernel/mac80211/files/lib/wifi/mac80211.sh
-mv $GITHUB_WORKSPACE/patch/immortalwrt-21.02/zz-diy-wrt1900 package/base-files/files/etc/uci-defaults/zz-diy
+mv $GITHUB_WORKSPACE/patch/immortalwrt-21.02/zz-diy-18.06 package/base-files/files/etc/uci-defaults/zz-diy
 sed -i 's#downloads.immortalwrt.org#mirror.nju.edu.cn/immortalwrt#g' package/emortal/default-settings/files/99-default-settings-chinese
 sed -i 's/ImmortalWrt/OpenWrt/g' include/version.mk
 
@@ -11,3 +11,10 @@ rm -rf feeds/luci/themes/luci-theme-argon
 git clone -b 18.06 --depth 1 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-eqos
 git clone --depth 1 https://github.com/wiwizcom/WiFiPortal.git package/WiFiPortal
+
+# iStore
+git clone --depth=1 -b main https://github.com/linkease/istore.git package/istore
+git clone --depth=1 -b master https://github.com/linkease/nas-packages.git package/nas-packages
+git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci.git package/nas-luci
+mv package/nas-packages/network/services/* package/nas-packages/
+rm -rf package/nas-packages/network
