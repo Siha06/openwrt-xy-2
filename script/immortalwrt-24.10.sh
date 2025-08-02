3aca20d332b45200b94d63bcc5bb1c6e5a203ab3
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-08-03 00:30:35 +0800

    Update immortalwrt-24.10.sh

--- script/immortalwrt-24.10.sh
+++ script/immortalwrt-24.10.sh
@@ -3,10 +3,10 @@ curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turbo
 # 修改默认IP，主机名
 sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
-sed -i 's/ImmortalWrt/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i 's/ImmortalWrt/OpenWrt/g' include/version.mk
-mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
+#sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+#sed -i 's/ImmortalWrt/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/ImmortalWrt/OpenWrt/g' include/version.mk
+#mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
 mv $GITHUB_WORKSPACE/patch/immortalwrt-24.10/zz-diy package/base-files/files/etc/uci-defaults/zz-diy
 
 #完全删除luci版本
