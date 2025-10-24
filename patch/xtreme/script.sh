7f15e6208b1b9d90030b28f129854fffd773817f
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-10-24 11:55:26 +0800

    Update script.sh

--- patch/xtreme/script.sh
+++ patch/xtreme/script.sh
@@ -5,12 +5,12 @@ sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_genera
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.23.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 sed -i 's/OpenWrt/Xtreme_Link/g' package/base-files/files/bin/config_generate
 
-#git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone --depth 1 -b openwrt-23.05 https://github.com/immortalwrt/luci.git package/imm23-luci
 rm -rf feeds/luci/modules
 mv package/imm23-luci/modules feeds/luci/modules
-mv package/imm23-luci/themes/luci-theme-argon package/luci-theme-argon
-sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' package/luci-theme-argon/Makefile
+#mv package/imm23-luci/themes/luci-theme-argon package/luci-theme-argon
+#sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' package/luci-theme-argon/Makefile
 rm -rf package/imm23-luci
 
 mv $GITHUB_WORKSPACE/patch/xtreme/zz-diy package/base-files/files/etc/uci-defaults/zz-diy
