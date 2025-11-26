41ea50c3fa6ea50defbf40c6c7e979e83c1f7e6f
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-11-26 20:55:27 +0800

    Create zz-x86.sh

---
+++ patch/immortalwrt-21.02/zz-x86.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+
+# 设置所有网口可访问网页终端
+uci delete ttyd.@ttyd[0].interface
+
+# 设置所有网口可连接 SSH
+uci set dropbear.@dropbear[0].Interface=''
+
+uci commit
+
+#sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
+#sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
+
+sed -i '/modem/d' /etc/opkg/distfeeds.conf
+sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
+sed -i 's#downloads.immortalwrt.org#mirrors.pku.edu.cn/immortalwrt#g' /etc/opkg/distfeeds.conf
+sed -i '/targets/d' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz core https://mirrors.pku.edu.cn/immortalwrt/releases/21.02-SNAPSHOT/targets/x86/64/packages' /etc/opkg/distfeeds.conf
+
+
+ # /etc/init.d/network restart
+
+exit 0
