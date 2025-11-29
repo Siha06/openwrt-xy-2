d295ada13a5d87692a0d9f6500dbd64536bdea50
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-11-29 20:31:18 +0800

    Create zz-diy.sh

---
+++ patch/openwrt/zz-diy.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+uci -q get system.@imm_init[0] > "/dev/null" || uci -q add system imm_init > "/dev/null"
+
+if ! uci -q get system.@imm_init[0].system_chn > "/dev/null"; then
+	uci -q batch <<-EOF
+		set system.@system[0].timezone="CST-8"
+		set system.@system[0].zonename="Asia/Shanghai"
+
+		delete system.ntp.server
+		add_list system.ntp.server="ntp.tencent.com"
+		add_list system.ntp.server="ntp1.aliyun.com"
+		add_list system.ntp.server="ntp.ntsc.ac.cn"
+		add_list system.ntp.server="cn.ntp.org.cn"
+
+		set system.@imm_init[0].system_chn="1"
+		commit system
+	EOF
+fi
+
+# 设置默认防火墙规则，方便虚拟机首次访问 WebUI
+#uci set firewall.@zone[1].input='ACCEPT'
+
+# 设置所有网口可访问网页终端
+uci delete ttyd.@ttyd[0].interface
+
+# 设置所有网口可连接 SSH
+uci set dropbear.@dropbear[0].Interface=''
+
+uci commit
+
+sed -i '/passwall/d' /etc/opkg/distfeeds.conf
+sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
+sed -i 's#downloads.openwrt.org#mirrors.pku.edu.cn/openwrt#g' /etc/opkg/distfeeds.conf
+sed -i '/targets/d' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz kmods https://mirrors.pku.edu.cn/openwrt/releases/23.05-SNAPSHOT/targets/mvebu/cortexa9/kmods/5.15.189-1-3afe63059a10f5d607124fe31dbdd9c7' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz others https://mirrors.pku.edu.cn/openwrt/releases/23.05-SNAPSHOT/targets/mvebu/cortexa9/packages' /etc/opkg/distfeeds.conf
+
+#sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
+#sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
+
+/etc/init.d/system restart
+# /etc/init.d/network restart
+
+exit 0
