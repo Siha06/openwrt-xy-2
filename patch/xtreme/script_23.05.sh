ed5d9b973eab44aa2e212c89244e6709c63f8655
Siha06 <65899583+Siha06@users.noreply.github.com>
2026-01-18 22:01:51 +0800

    Update script_23.05.sh

--- patch/xtreme/script_23.05.sh
+++ patch/xtreme/script_23.05.sh
@@ -13,7 +13,7 @@ mv package/imm23-luci/themes/luci-theme-argon package/luci-theme-argon
 sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' package/luci-theme-argon/Makefile
 rm -rf package/imm23-luci
 
-mv $GITHUB_WORKSPACE/patch/xtreme/zz-diy package/base-files/files/etc/uci-defaults/zz-diy
+mv $GITHUB_WORKSPACE/patch/xtreme/23.05/zz-diy package/base-files/files/etc/uci-defaults/zz-diy
 mv $GITHUB_WORKSPACE/patch/xtreme/23.05/10_system.js feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 mv $GITHUB_WORKSPACE/patch/xtreme/23.05/mac80211.sh package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
